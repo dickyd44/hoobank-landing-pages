89bb887488412bd8ee6c0e499fbd55bf43bfea2e
dikydrmwn08@gmail.com <dikydrmwn08@gmail.com>
2024-04-08 01:22:03 +0800

    set logo title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/hoobank.svg" sizes="any" />
       <body className={poppins.className}>{children}</body>
     </html>
   );
