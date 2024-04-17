920fe54c63a7323ebb4486917ae39a38423cadb3
dikydrmwn08@gmail.com <dikydrmwn08@gmail.com>
2024-04-17 23:39:53 +0800

    add transition to hero section

--- src/components/organisms/HeroSection/index.tsx
+++ src/components/organisms/HeroSection/index.tsx
@@ -1,28 +1,69 @@
 "use client";
 
 import { GetStarted } from "@/components/atoms";
-import styles from "@/app/style";
 import { discount, robot } from "@/assets";
+import { animate, motion, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+import styles from "@/app/style";
 import Image from "next/image";
 
 export default function HeroSection() {
+  const discountRef: any = useRef(null);
+  const isInView = useInView(discountRef);
+
+  useEffect(() => {
+    const controls = animate(0, 20, {
+      duration: 1.2,
+      onUpdate(value) {
+        discountRef.current.textContent = `$${value.toFixed()}`;
+      },
+    });
+    return () => controls.stop();
+  }, [isInView]);
+
+  const heroContent = {
+    visible: { x: 0, opacity: 1, transition: { duration: 0.5 } },
+    hidden: { x: -100, opacity: 0 },
+  };
+
+  const labelContent = {
+    visible: {
+      scale: 1,
+      x: 0,
+      y: 0,
+      transition: { duration: 0.5 },
+    },
+    hidden: { x: -100 },
+  };
+
+  const imageContent = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.5, staggerChildren: 0.1, delayChildren: 0.1 },
+    },
+    hidden: { opacity: 0, x: 200 },
+  };
+
   return (
-    <section
+    <motion.section
+      variants={heroContent}
+      animate="visible"
+      initial="hidden"
       id="home"
       className={`flex md:flex-row flex-col ${styles.paddingY}`}
     >
-      <div
+      <motion.div
+        variants={labelContent}
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-10">
-          <Image
-            src={discount}
-            alt="discount"
-            className="w-[32px] h-[32px] hero-gsap"
-          />
+          <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">20%</span> Discount For{" "}
-            <span className="text-white">1 Month</span> Account
+            <span ref={discountRef} className="text-white">
+              0%
+            </span>{" "}
+            Discount For <span className="text-white">1 Month</span> Account
           </p>
         </div>
 
@@ -44,9 +85,12 @@ export default function HeroSection() {
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
         </p>
-      </div>
+      </motion.div>
 
-      <div
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={imageContent}
         className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
       >
         <Image
@@ -60,11 +104,11 @@ export default function HeroSection() {
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
-      </div>
+      </motion.div>
 
       <div className={`ss:hidden ${styles.flexCenter}`}>
         <GetStarted />
       </div>
-    </section>
+    </motion.section>
   );
 }
